cb011cc393a0a5e6ddd35cf76936b546c813cdd6
David Whitaker <damwhit@gmail.com>
2019-02-12 15:50:39 -0700

    Write implementations for put and delete

--- server.js
+++ server.js
@@ -1,9 +1,9 @@
 import express from 'express';
 const app = express();
-//
+
 import cors from 'cors';
 app.use(cors());
-//
+
 app.use(express.json());
 app.set('port', 3001);
 
@@ -26,14 +26,7 @@ app.get('/api/v1/pets', (req, res) => {
   res.status(200).json(app.locals.pets);
 });
 
-app.get('/api/v1/pets/:id', (req, res) => {
-  const pet = app.locals.pets.find(pet => pet.id == req.params.id);
-  if (!pet) return res.status(404).json('Pet not found');
-  return res.status(200).json(pet);
-});
-
 app.post('/api/v1/pets', (req, res) => {
-  // body needs name and type
   const { name, type } = req.body;
   if (!name || !type) return res.status(422).json('Please provide a name and a type');
   const newPet = {
@@ -44,3 +37,36 @@ app.post('/api/v1/pets', (req, res) => {
   res.status(201).json(newPet);
 });
 
+app.get('/api/v1/pets/:id', (req, res) => {
+  const pet = app.locals.pets.find(pet => pet.id == req.params.id);
+  if (!pet) return res.status(404).json('Pet not found');
+  return res.status(200).json(pet);
+});
+
+app.put('/api/v1/pets/:id', (req, res) => {
+  const { name, type } = req.body;
+  const { id } = req.params;
+  let petWasFound = false;
+  const newPets = app.locals.pets.map(pet => {
+    if (pet.id == id) {
+      petWasFound = true;
+      return { id, name, type };
+    } else {
+      return pet;
+    }
+  });
+
+  if (!name || !type) return res.status(422).json('Please provide a name and a type');
+  if (!petIndex) return res.status(404).json('Pet not found');
+
+  app.locals.pets = newPets;
+  return res.sendStatus(204);
+});
+
+app.delete('/api/v1/pets/:id', (req, res) => {
+  const petIndex = app.locals.pets.findIndex(pet => pet.id == req.params.id);
+  if (petIndex === -1) return res.status(404).json('Pet not found');
+
+  app.locals.pets.splice(petIndex, 1);
+  return res.sendStatus(204);
+});
