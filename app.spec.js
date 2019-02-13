4526c9b21dbae9fdacaa67e892547814f7b81289
David Whitaker <damwhit@gmail.com>
2019-02-13 08:31:46 -0700

    more stuff

--- app.spec.js
+++ app.spec.js
@@ -3,14 +3,8 @@ import '@babel/polyfill';
 import app from './app';
 
 describe('get /api/v1/pets', () => {
-  // Three things we need to test:
-  // response status code
-  // response text
-  // any changes to our data set that should occur
-
-  it('should return a 200', () => {
-    request(app).get('/api/v1/pets').then((res) => {
-      expect(res.statusCode).toBe(200);
-    });
+  it('should return a 200', async () => {
+    const response = await request(app).get('/api/v1/pets');
+    expect(response.statusCode).toBe(200);
   });
 });
